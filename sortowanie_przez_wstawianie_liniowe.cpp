6e6be0ddefced66ea66b180250bdb9ae2d42a3b2
Kaczymorda <jak.kurc@gmail.com>
2021-02-27 13:09:48 +0100

    commit on main

---
+++ sortowanie_przez_wstawianie_liniowe.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+void sortowanie(int *tab, int n) {
+    int element = 0, j = 0;
+
+    for(int i = 1; i < n; i++) {
+        element = tab[i];
+
+        j = i - 1;
+
+        while(j >= 0 && tab[j] > element) {
+            tab[j + 1] = tab[j];
+
+            j--;
+        }
+
+        tab[j + 1] = element;
+    }
+}
+
+int main(int argc, char** argv) {
+    int n = 0; //ilosc elementow tablicy
+
+    while(1) {
+        std::cout << "Podaj ilosc elementow tablicy: "; std::cin >> n;
+
+        if(n > 0) break;
+    }
+
+    int tab[n];
+
+    for(int i = 0; i < n; i++) {
+        std::cout << "Podaj " << i << " element tablicy: "; 
+        std::cin >> tab[i];
+    }
+
+    sortowanie(tab, n);
+
+    for(int i = 0; i < n; i++) {
+        std::cout << tab[i] << " "; 
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
