58817b813fe56a752482d16fb689a8b685fec6ad
K-u-r-c <jak.kurc@gmail.com>
2021-03-09 14:07:10 +0100

    turbo fibbonacci ++

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -4,10 +4,10 @@
 #include <iomanip>
 
 void fibonacci(int n) {
-    double a = 0, b = 1;
+    long double a = 0, b = 1;
 
-    for(int i = 0; i < n; i++) {
-        std::cout << std::setprecision(0) << std::fixed << b << " \n";
+    for(int i = 0; i <= n; i++) {
+        std::cout << i << ": " << std::setprecision(0) << std::fixed << b << " \n";
         b += a;
         a = b - a;
     }
@@ -19,7 +19,7 @@ int main(int argc, char** argv) {
     while(1) {
         std::cout << "Podaj ilosc wyrazow ciagu: "; std::cin >> n;
 
-        if(n <= 1476) break;
+        if(n <= 23600) break;
     }
 
     fibonacci(n);
